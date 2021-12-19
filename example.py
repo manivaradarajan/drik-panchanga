54388d7216f03c168c1f5f38adbe3c7676590b80
Mani Varadarajan <manimani@gmail.com>
2021-12-18 19:19:56 -0800

    After black

--- example.py
+++ example.py
@@ -6,6 +6,7 @@ import tzlocal
 
 import panchanga
 
+
 def _panchanga_date_to_datetime(date, base_datetime):
     # The input date and time elements in the tuple are expressed as floats.
     # Convert the float hour into integer hours, minutes and seconds.
@@ -49,7 +50,6 @@ def moonset(time, place):
     moonset_jd, _ = panchanga.moonset(jd, place)
     d = panchanga.jd_to_gregorian(moonset_jd)
     return _panchanga_date_to_datetime(d, time)
-
 
 
 def tithi(time, place):
@@ -96,34 +96,29 @@ def raasi(time):
 
 
 if __name__ == "__main__":
-    f = open('sanskrit_names.json')
+    f = open("sanskrit_names.json")
     names = json.load(f)
 
     local_tz = tzlocal.get_localzone()
-    #print(local_tz)
-    now = datetime.datetime.now(local_tz) #- datetime.timedelta(days=30*8)
+    # print(local_tz)
+    now = datetime.datetime.now(local_tz)  # - datetime.timedelta(days=30*8)
     print("Calculating for", now)
-    tz_offset = local_tz.utcoffset(now).total_seconds()/60/60
+    tz_offset = local_tz.utcoffset(now).total_seconds() / 60 / 60
     palo_alto = panchanga.Place(37.468319, -122.143936, tz_offset)
 
-    print("Samvatsara", names['samvats'][str(samvatsara(now, palo_alto))])
+    print("Samvatsara", names["samvats"][str(samvatsara(now, palo_alto))])
     m = maasa(now, palo_alto)
-    print("Maasa", names['masas'][str(m[0])], 'adhika' if m[1] else '')
-    print("Ritu", names['ritus'][str(ritu(now, palo_alto))])
+    print("Maasa", names["masas"][str(m[0])], "adhika" if m[1] else "")
+    print("Ritu", names["ritus"][str(ritu(now, palo_alto))])
     t = tithi(now, palo_alto)
-    print("Tithi", names['tithis'][str(t[0])], 'ends', t[1])
-    print("Vaara", names['varas'][str(vaara(now))])
+    print("Tithi", names["tithis"][str(t[0])], "ends", t[1])
+    print("Vaara", names["varas"][str(vaara(now))])
     n = nakshatra(now, palo_alto)
-    print("Nakshatra", names['nakshatras'][str(n[0])], 'ends', n[1])
+    print("Nakshatra", names["nakshatras"][str(n[0])], "ends", n[1])
     r = raasi(now)
-    print("Raasi", names['raasis'][str(r)])
+    print("Raasi", names["raasis"][str(r)])
 
     print("Sunrise", sunrise(now, palo_alto))
     print("Sunset", sunset(now, palo_alto))
-    #print("Moonrise", moonrise(now, palo_alto))
-    #print("Moonset", moonset(now, palo_alto))
-
-
-
-
-
+    # print("Moonrise", moonrise(now, palo_alto))
+    # print("Moonset", moonset(now, palo_alto))
